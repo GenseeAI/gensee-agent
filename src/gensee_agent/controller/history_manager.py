04b05f96d6d1afb2e4d1b076f3cc6f2481f33cf3
Shengqi Zhu <shengqi@gensee.ai>
2025-11-14 01:47:02 -0800

    Refactor for history management.

--- src/gensee_agent/controller/history_manager.py
+++ src/gensee_agent/controller/history_manager.py
@@ -1,3 +1,4 @@
+import traceback
 import aiofiles
 from datetime import datetime
 import json
@@ -7,7 +8,10 @@ from dataclasses import asdict, is_dataclass
 from redis.asyncio import Redis, RedisCluster
 
 from gensee_agent.utils.configs import BaseConfig, register_configs
+from gensee_agent.utils.logging import configure_logger
 from gensee_agent.controller.dataclass.llm_use import LLMUse
+
+logger = configure_logger(__name__)
 
 class CustomJSONEncoder(json.JSONEncoder):
     def default(self, o):
@@ -49,30 +53,54 @@ class HistoryManager:
         self.history = []
 
     async def add_entry(self, name: str, title: str, entry: Any):
-        entry = {
+        history_entry = {
             "name": name,
             "title": title,
             "entry": entry
         }
-        self.history.append(entry)
+        self.history.append(history_entry)
         if self.dump_path is not None:
             async with aiofiles.open(self.dump_path, "w") as f:
                 await f.write(json.dumps(self.history, indent=2, cls=CustomJSONEncoder))
         if self.redis_client is not None and self.session_id is not None:
             # Only need to store the last entry for "llm_use" type in Redis
             if name == "llm_use":
-                await self.redis_client.set(self.session_id, json.dumps(entry, cls=CustomJSONEncoder, separators=(',', ':'), indent=None) + "\n")
+                await self.redis_client.set(self.session_id, json.dumps(history_entry, cls=CustomJSONEncoder, separators=(',', ':'), indent=None) + "\n")
 
-    async def read_history(self) -> bool:
+    async def get_history(self) -> Optional[dict]:
+        """
+        Return value: dict of "name", "title", and "entry".  Entry will be a plain dict, not LLMUse class.
+        """
         if self.redis_client is None or self.session_id is None:
-            return False
+            return None
         data = await self.redis_client.get(self.session_id)
         if data is None:
-            return False
-        entry = json.loads(data)
-        entry["entry"] = LLMUse(**entry["entry"])
-        self.history = [entry]
-        return True
+            return None
+        history_entry = json.loads(data)
+        return history_entry
+
+    async def read_history(self) -> bool:
+        """
+        Returns True if history exists and is loaded, False otherwise.
+        """
+        history_entry = await self.get_history()
+        if history_entry is not None:
+            history_entry["entry"] = LLMUse(**history_entry["entry"])
+            self.history = [history_entry]
+            return True
+        return False
+
+    async def set_history(self, history: dict):
+        """
+        history: dict of "name", "title", and "entry".  Entry will be a plain dict, not LLMUse class.
+        """
+        if await self.get_history() is not None:
+            # Don't overwrite existing history
+            return
+        if self.redis_client is not None and self.session_id is not None:
+            await self.redis_client.set(self.session_id, json.dumps(history, separators=(',', ':'), indent=None) + "\n")
+        history["entry"] = LLMUse(**history["entry"])
+        self.history = [history]
 
     def get_last_entry_of_type(self, name: str) -> Any:
         for record in reversed(self.history):
--- src/gensee_agent/utils/streaming_data.py
+++ src/gensee_agent/utils/streaming_data.py
@@ -2,6 +2,9 @@ import datetime
 from typing import Any, Literal, Optional
 from pydantic import BaseModel, Field
 from pydantic import field_validator
+import shortuuid
+
+STREAMING_MESSAGE_TYPES = Literal["status", "assistant", "error", "document", "internal", "user", "metadata"]
 
 class QuestionData(BaseModel):
     id: Optional[str]
@@ -13,24 +16,27 @@ class StreamingUserInteraction(BaseModel):
     type: Literal["user_interaction", "user_notification"]
     questions: list[QuestionData]
     buttons: list[str]
-    conversation_id: str
+    interaction_id: str
+    cls_info: Literal["StreamingUserInteraction"] = Field(frozen=True, default="StreamingUserInteraction")
 
 class DocumentAction(BaseModel):
     type: Literal["update_doc"]
     pad_id: Optional[str] = None  # ID of the pad on etherpad
     doc_id: Optional[str] = None  # ID of the document, e.g., "research_report".  Originally called target.
     title: Optional[str] = None
+    cls_info: Literal["DocumentAction"] = Field(frozen=True, default="DocumentAction")
 
 class InternalAction(BaseModel):
     type: Literal["hand_over", "append_context"]
     target_agent: str  # e.g., "short_answer_agent", "deep_research_agent"
     params: dict[str, Any] = {}
+    cls_info: Literal["InternalAction"] = Field(frozen=True, default="InternalAction")
 
 class StreamingMessage(BaseModel):
-    type: Literal["status", "assistant", "error", "document", "internal"]
+    type: STREAMING_MESSAGE_TYPES  # User is used for user messages logged in the session
     delta: str | dict
     datatype: Literal["str", "json"] = "str"
-    obj_type: Optional[str] = None
+    obj_type: Optional[str] = None  # obj_data can be not None only with "metadata" type.
     action: Optional[StreamingUserInteraction | DocumentAction | InternalAction] = None
 
     @field_validator('action')
@@ -40,9 +46,20 @@ class StreamingMessage(BaseModel):
             raise ValueError('action can only have values when type is "assistant", "document", or "internal"')
         return v
 
+    # @field_validator('obj_type')
+    # @classmethod
+    # def validate_obj_type(cls, v, info):
+    #     if v is not None and info.data.get('type') not in ['metadata', 'assistant']:
+    #         raise ValueError('obj_type can only have a value when type is "metadata"')
+    #     return v
+
+def new_conversation() -> str:
+    return f"conv.{shortuuid.uuid()}"
+
 class StreamingData(BaseModel):
     type: Literal["start", "delta", "end"]
-    session_id: str
+    session_id: str  # Matches the session_id in the request
+    conversation_id: str  # If response belongs to the same conversation (e.g., word tokens), they will share the same conversation_id
     timestamp: str = Field(default_factory=lambda: datetime.datetime.now(datetime.UTC).isoformat())
     message: Optional[StreamingMessage] = None
 
@@ -65,47 +82,68 @@ class StreamingData(BaseModel):
             raise ValueError("Invalid streaming output format.")
 
     @classmethod
-    def start(cls, session_id: str) -> "StreamingData":
-        return cls(type="start", session_id=session_id)
+    def start(cls, session_id: str, conversation_id: Optional[str] = None) -> "StreamingData":
+        if conversation_id is None:
+            conversation_id = new_conversation()
+        return cls(type="start", session_id=session_id, conversation_id=conversation_id)
 
     @classmethod
-    def end(cls, session_id: str) -> "StreamingData":
-        return cls(type="end", session_id=session_id)
+    def end(cls, session_id: str, conversation_id: Optional[str] = None) -> "StreamingData":
+        if conversation_id is None:
+            conversation_id = new_conversation()
+        return cls(type="end", session_id=session_id, conversation_id=conversation_id)
 
     @classmethod
-    def _simple_message(cls, session_id: str, message: str | dict, message_type: Literal["status", "assistant", "error", "document", "internal"],
+    def _simple_message(cls, session_id: str, conversation_id: Optional[str], message: str | dict, message_type: STREAMING_MESSAGE_TYPES,
                         obj_type: Optional[str], *, action: Optional[StreamingUserInteraction] = None) -> "StreamingData":
+        if conversation_id is None:
+            conversation_id = new_conversation()
         if isinstance(message, str):
             return cls(
-                type="delta", session_id=session_id, message=StreamingMessage(type=message_type, delta=message, datatype="str", obj_type=obj_type, action=action))
+                type="delta", session_id=session_id, conversation_id=conversation_id,
+                message=StreamingMessage(type=message_type, delta=message, datatype="str", obj_type=obj_type, action=action)
+            )
         elif isinstance(message, dict):
             return cls(
-                type="delta", session_id=session_id, message=StreamingMessage(type=message_type, delta=message, datatype="json", obj_type=obj_type, action=action))
+                type="delta", session_id=session_id, conversation_id=conversation_id,
+                message=StreamingMessage(type=message_type, delta=message, datatype="json", obj_type=obj_type, action=action)
+            )
         else:
             raise ValueError("Message must be either str or dict.")
 
     @classmethod
-    def status(cls, session_id: str, message: str | dict, obj_type: Optional[str] = None) -> "StreamingData":
-        return cls._simple_message(session_id, message, "status", obj_type)
+    def status(cls, session_id: str, message: str | dict, conversation_id: Optional[str] = None, obj_type: Optional[str] = None) -> "StreamingData":
+        return cls._simple_message(session_id, conversation_id, message, "status", obj_type)
 
     @classmethod
-    def assistant(cls, session_id: str, message: str | dict, obj_type: Optional[str] = None, *, action: Optional[StreamingUserInteraction] = None) -> "StreamingData":
-        return cls._simple_message(session_id, message, "assistant", obj_type, action=action)
+    def assistant(cls, session_id: str, message: str | dict, conversation_id: Optional[str] = None, obj_type: Optional[str] = None, *, action: Optional[StreamingUserInteraction] = None) -> "StreamingData":
+        return cls._simple_message(session_id, conversation_id, message, "assistant", obj_type, action=action)
 
     @classmethod
-    def error(cls, session_id: str, message: str, obj_type: Optional[str] = None) -> "StreamingData":
-        return cls._simple_message(session_id, message, "error", obj_type)
-
+    def error(cls, session_id: str, message: str, conversation_id: Optional[str] = None, obj_type: Optional[str] = None) -> "StreamingData":
+        return cls._simple_message(session_id, conversation_id, message, "error", obj_type)
     @classmethod
-    def document(cls, session_id: str, document_change: str, *, action: Optional[DocumentAction] = None) -> "StreamingData":
+    def document(cls, session_id: str, document_change: str, *, conversation_id: Optional[str] = None, action: Optional[DocumentAction] = None) -> "StreamingData":
         # If document is empty, it means to force an update on the document.
+        if conversation_id is None:
+            conversation_id = new_conversation()
         return cls(
-            type="delta", session_id=session_id, message=StreamingMessage(type="document", delta=document_change, datatype="str", action=action))
+            type="delta", session_id=session_id, conversation_id=conversation_id, message=StreamingMessage(type="document", delta=document_change, datatype="str", action=action))
 
     @classmethod
-    def internal(cls, session_id: str, *, action: Optional[InternalAction] = None) -> "StreamingData":
+    def user(cls, session_id: str, message: str, conversation_id: Optional[str] = None, obj_type: Optional[str] = None) -> "StreamingData":
+        return cls._simple_message(session_id, conversation_id, message, "user", obj_type)
+
+    @classmethod
+    def internal(cls, session_id: str, *, conversation_id: Optional[str] = None, action: Optional[InternalAction] = None) -> "StreamingData":
+        if conversation_id is None:
+            conversation_id = new_conversation()
         return cls(
-            type="delta", session_id=session_id, message=StreamingMessage(type="internal", delta="", datatype="str", action=action))
+            type="delta", session_id=session_id, conversation_id=conversation_id, message=StreamingMessage(type="internal", delta="", datatype="str", action=action))
+
+    @classmethod
+    def metadata(cls, session_id: str, metadata: dict, conversation_id: Optional[str] = None, obj_type: Optional[str] = None) -> "StreamingData":
+        return cls._simple_message(session_id, conversation_id, metadata, "metadata", obj_type)
 
     # @classmethod
     # def delta_assistant(cls, session_id: str, message: str, action: dict, *, target: Optional[str] = None) -> str:
